cefbebf7e84d03ed73678e013858ec1783b40619
Malak Abdalrahim <malek_4693@limu.edu.ly>
2025-12-22 12:01:44 +0200

    logout

--- routes/web.php
+++ routes/web.php
@@ -15,58 +15,6 @@ Route::get('/', function () {
 });
 
 
-
-Route::resource('student', studentController::class)->names([
-    'index' => 'student.index',
-    'show' => 'student.show',
-    'create' => 'student.create',
-    'update' => 'student.update',
-    'edit' => 'student.edit',
-    'store' => 'student.store',
-    'destroy' => 'student.destroy'
-]);
-
-Route::resource('course', courseController::class)->names([
-    'index'=>'course.index',
-    'show'=>'course.show',
-    'create'=>'course.create',
-    'update'=>'course.update',
-    'edit'=>'course.edit',
-    'store'=>'course.store',
-    'destroy'=>'course.destroy'
-]);
-Route::resource('professor', professorController::class)->names([
-    'index'=>'professor.index',
-    'show'=>'professor.show',
-    'create'=>'professor.create',
-    'update'=>'professor.update',
-    'edit'=>'professor.edit',
-    'store'=>'professor.store',
-    'destroy'=>'professor.destroy'
-]);
-
-
-Route::resource('department', departmentController::class)->names([
-    'index'=>'department.index',
-    'show'=>'department.show',
-    'create'=>'department.create',
-    'update'=>'department.update',
-    'edit'=>'department.edit',
-    'store'=>'department.store',
-    'destroy'=>'department.destroy'
-]);
-Route::resource('enrollment', enrollmentController::class)->names([
-    'index'=>'enrollment.index',
-    'show'=>'enrollment.show',
-    'create'=>'enrollment.create',
-    'update'=>'enrollment.update',
-    'edit'=>'enrollment.edit',
-    'store'=>'enrollment.store',
-    'destroy'=>'enrollment.destroy'
-]);
-
-Route::get('dashboard', [dashboardcontroller::class,'index'])->name('dashboard');
-
 Route::get('/', [homeController::class,'index'])->name('home.index');
 Route::get('/about', [homeController::class,'about'])->name('home.about');
 Route::get('/contact', [homeController::class,'contact'])->name('home.contact');
@@ -74,4 +22,56 @@ Route::get('/contact', [homeController::class,'contact'])->name('home.contact');
 
 Route::get('/adminLogin', [authController::class,'adminLogin'])->name('admin.login');
 Route::post('/adminLogin', [authController::class,'adminCheckLogin'])->name('admin.adminCheckLogin');
+
+Route::middleware(middleware:'auth:admin')->group(callback:function():void{
+
+    Route::get('dashboard', [dashboardcontroller::class,'index'])->name('dashboard');
+
+    Route::resource('student', studentController::class)->names([
+    'index' => 'student.index',
+    'show' => 'student.show',
+    'create' => 'student.create',
+    'update' => 'student.update',
+    'edit' => 'student.edit',
+    'store' => 'student.store',
+    'destroy' => 'student.destroy']);
+
+    Route::resource('course', courseController::class)->names([
+    'index'=>'course.index',
+    'show'=>'course.show',
+    'create'=>'course.create',
+    'update'=>'course.update',
+    'edit'=>'course.edit',
+    'store'=>'course.store',
+    'destroy'=>'course.destroy']);
+
+    Route::resource('professor', professorController::class)->names([
+    'index'=>'professor.index',
+    'show'=>'professor.show',
+    'create'=>'professor.create',
+    'update'=>'professor.update',
+    'edit'=>'professor.edit',
+    'store'=>'professor.store',
+    'destroy'=>'professor.destroy']);
+
+    Route::resource('department', departmentController::class)->names([
+    'index'=>'department.index',
+    'show'=>'department.show',
+    'create'=>'department.create',
+    'update'=>'department.update',
+    'edit'=>'department.edit',
+    'store'=>'department.store',
+    'destroy'=>'department.destroy']);
+
+    Route::resource('enrollment', enrollmentController::class)->names([
+    'index'=>'enrollment.index',
+    'show'=>'enrollment.show',
+    'create'=>'enrollment.create',
+    'update'=>'enrollment.update',
+    'edit'=>'enrollment.edit',
+    'store'=>'enrollment.store',
+    'destroy'=>'enrollment.destroy']);
+    
+
+});
 
